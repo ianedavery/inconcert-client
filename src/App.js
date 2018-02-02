0d41a790dd1143e0a70d14cc87f64117c7b77e60
Ian Avery <ianedavery@gmail.com>
2018-02-02 11:37:28 -0700

    added App test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import LandingPage from './components/LandingPage';
 
 export default function App() {
     return (
-      <LandingPage /> 
+    	<Router>
+      		<Route exact path='/' component={LandingPage} />
+      	</Router> 
     );
 }
