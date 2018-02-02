ebb24a03f7faf94e38da59b24f701fde9262668b
Ian Avery <ianedavery@gmail.com>
2018-02-02 11:43:31 -0700

    added shallow tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
 
---
+++ src/components/LandingPage.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import LandingPage from './LandingPage';
+
+describe('<LandingPage />', () => {
+	it('renders without crashing', () => {
+		shallow(<LandingPage />);
+	});
+});
