ac15e3e0ec26b28a09127ee66e7d0a594d9112c9
Ian Avery <ianedavery@gmail.com>
2018-02-18 12:03:43 -0700

    added edit toggle button

--- src/components/RecipieName.js
+++ src/components/RecipieName.js
@@ -5,6 +5,17 @@ import {fetchRecipieDetails} from '../actions/recipieDetails';
 import {deleteRecipie} from '../actions/deleteRecipie';
 
 export class RecipieName extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {isEditing: false},
+		this.toggleEdit = this.toggleEdit.bind(this)
+	}
+
+	toggleEdit() {
+		this.setState({
+			isEditing: !this.state.isEditing
+		})
+	}
 
 	componentWillMount() {
 		this.props.dispatch(fetchRecipieDetails(this.props.id));
@@ -16,6 +27,17 @@ export class RecipieName extends React.Component {
 	}
 
 	render() {
+
+		if(this.state.isEditing) {
+			return(
+				<div>
+					<div>
+						edit recipie
+					</div>
+					<button type='button' onClick={this.toggleEdit}>cancel</button>
+				</div>
+			)
+		}
 
 		let newArray = this.props.recipie.ingredients;
 
@@ -43,6 +65,7 @@ export class RecipieName extends React.Component {
 		        	<p>{this.props.recipie.instructions}</p>
 	        	</section>
 	        	<button type='button' onClick={id => {if(window.confirm('Are you sure you want to delete?')) {this.deleteRecipie(id)};}}>Delete Recipie</button>
+	        	<button type='button' onClick={this.toggleEdit}>edit</button>
 	        </div>
 		)
 	}
